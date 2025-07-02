72473ebfcb43c93bc470ec4857f928ee3f397eb7
seokyoung <nsy7160@gmail.com>
2025-07-02 20:36:43 +0900

    feat:webSocket

--- src/main/java/com/meossamos/smore/domain/chat/videochat/controller/ApiV1SignallingController.java
+++ src/main/java/com/meossamos/smore/domain/chat/videochat/controller/ApiV1SignallingController.java
@@ -1,6 +1,5 @@
 package com.meossamos.smore.domain.chat.videochat.controller;
 
-import com.meossamos.smore.domain.chat.videochat.dto.SignalMessageDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -9,6 +8,8 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.util.Map;
+
 @Slf4j
 @RequiredArgsConstructor
 @Controller
@@ -16,16 +17,28 @@ public class ApiV1SignallingController {
     private final SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/signal/send/{roomId}")
-    public void handleSignalMessage(@DestinationVariable String roomId,
-                                    @Payload SignalMessageDto message) {
-        log.info("📥 받은 메시지 (roomId: {}): {}", roomId, message);
-
-        // 테스트용으로 pong 응답
-        if ("ping".equals(message.getType())) {
-            SignalMessageDto pong = new SignalMessageDto("pong", "서버 응답: pong!");
-            simpMessagingTemplate.convertAndSend("/topic/signal/" + roomId, pong);
-            log.info("📤 pong 전송 완료");
-        }
+    public void signal(
+            @DestinationVariable String roomId,
+            @Payload Map<String, Object> message
+            ) {
+            System.out.println("📨 Signal Received: " + message);
+            simpMessagingTemplate.convertAndSend("/topic/signal/" + roomId, message);
     }
+
+/* 테스트 */
+//    @MessageMapping("/signal/send/{roomId}")
+//    public void handleSignalMessage(@DestinationVariable String roomId,
+//                                    @Payload SignalMessageDto message) {
+//        log.info("📥 받은 메시지 (roomId: {}): {}", roomId, message);
+//
+//        // 테스트용으로 pong 응답
+//        if ("ping".equals(message.getType())) {
+//            SignalMessageDto pong = new SignalMessageDto("pong", "서버 응답: pong!");
+//            simpMessagingTemplate.convertAndSend("/topic/signal/" + roomId, pong);
+//            log.info("📤 pong 전송 완료");
+//        }
+//    }
+
+
 }
 
